f886c4d94b915665289c97090facaeb2a9687169
Glen Kaukola <gkaukola@cs.ucr.edu>
2013-01-12 12:51:58 -0800

    more sicp

--- 1_3.scm
+++ 1_3.scm
@@ -1,8 +1,8 @@
-(define (sum_of_squares_of_largest n1 n2 n3)
-  (cond ((and (> n1 n3) (> n2 n3)) (sum_of_squares n1 n2))
-        ((and (> n1 n2) (> n3 n2)) (sum_of_squares n1 n3))
-        (else (sum_of_squares n2 n3))))
+(define (sum-of-squares-of-largest n1 n2 n3)
+  (cond ((and (> n1 n3) (> n2 n3)) (sum-of-squares n1 n2))
+        ((and (> n1 n2) (> n3 n2)) (sum-of-squares n1 n3))
+        (else (sum-of-squares n2 n3))))
 
-(define (sum_of_squares n1 n2) (+ (square n1) (square n2)))
+(define (sum-of-squares n1 n2) (+ (square n1) (square n2)))
 
 (define (square n) (* n n))
