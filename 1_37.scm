f3a4e02449a43b50fa380569f9360c2361597c3d
Glen Kaukola <gkaukola@cs.ucr.edu>
2013-02-01 17:49:43 -0800

    1.37 etc.

---
+++ 1_37.scm
@@ -0,0 +1,27 @@
+(define (cont-frac n d k)
+  (define (iter i)
+    (if (= i k)
+        (/ (n i) (d i))
+        (/ (n i) (+ (d i) (iter (+ i 1))))))
+  (iter 1))
+
+(define (golden-ratio k)
+  (/ 1
+     (cont-frac (lambda (i) 1.0)
+                (lambda (i) 1.0)
+                 k)))
+
+(define (golden-ratio-iter k)
+  (/ 1
+     (cont-frac-iter (lambda (i) 1.0)
+                     (lambda (i) 1.0)
+                      k)))
+
+(define (cont-frac-iter n d k)
+  (define (iter result k)
+    (if (= k 0)
+        result
+        (iter (/ (n k)
+                 (+ (d k) result))
+              (- k 1))))
+  (iter 0 k))
