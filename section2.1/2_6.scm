7b9c5c71a822e53b20a63887c6b5af80160c1b75
Glen Kaukola <gkaukola@cs.ucr.edu>
2013-02-06 18:04:41 -0800

    church numbers

--- section2.1/2_6.scm
+++ section2.1/2_6.scm
@@ -4,4 +4,12 @@
   (lambda (f) (lambda (x) (f ((n f) x)))))
 
 (define one
-  (lambda (f) (lambda (x) (f (((lambda (g) (lambda (y) y))) f) x)))))
+  (lambda (f) (lambda (x) (f x))))
+
+(define two
+  (lambda (f) (lambda (x) (f (f x)))))
+
+(define (inc n) (+ n 1))
+
+(define (+ a b)
+  (lambda (f) (lambda (x) ((a f) ((b f) x)))))
