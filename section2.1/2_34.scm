df4d211d3cfcf4743d207f6618d90b5e8bbabe0d
Glen Kaukola <gkaukola@cs.ucr.edu>
2013-02-15 17:28:21 -0800

    accumulate

--- section2.1/2_34.scm
+++ section2.1/2_34.scm
@@ -1,4 +1,10 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
 (define (horner-eval x coefficient-sequence)
-  (accumulate (lambda (this-coeff higher-terms) (+ higher-terms )
+  (accumulate (lambda (this-coeff higher-terms) (+ this-coeff (* higher-terms x)))
               0
               coefficient-sequence))
