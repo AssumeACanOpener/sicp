6bddb578ce79b90b4ea5a828e344fc6eea0e1ca3
Glen Kaukola <gkaukola@cs.ucr.edu>
2013-03-03 05:24:06 -0800

    too damn tired to go any further

--- section2.1/2_18.scm
+++ section2.1/2_18.scm
@@ -1,11 +1,7 @@
-(define (reverse forward-list)
-  (define (iter forward-list reverse-list)
-    (if (null? forward-list)
-        reverse-list
-        (iter (cdr forward-list)
-              (cons (car forward-list)
-                    reverse-list))))
-
-  (iter forward-list (list)))
-
 (define a (list 1 2 3 4))
+
+(define (reverse s)
+  (if (null? (cdr s))
+      s
+      (append (reverse (cdr s))
+              (list (car s))))
---
+++ workspace.scm
@@ -0,0 +1,80 @@
+(define nil '())
+
+(define s (list 1 2 3 4 5))
+
+(define (last-pair s)
+  (if (null? (cdr s))
+      s
+      (last-pair (cdr s))))
+
+(define (reverse s)
+      (if (null? (cdr s))
+          s
+          (append (reverse (cdr s)) (list (car s)))))
+
+
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (no-more? coins)
+  (if (null? coins)
+      true
+      false))
+
+(define (first-denomination coins)
+  (car coins))
+
+(define (except-first-denomination coins)
+  (cdr coins))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
+
+(define (same-parity first . rest)
+  (define (parity-matches? x y)
+    (or (and (even? x)
+             (even? y))
+        (and (odd? x)
+             (odd? y))))
+  (define (parity-list s)
+    (if (null? s)
+        s
+        (if (parity-matches? first (car s))
+            (append (list (car s)) (parity-list (cdr s)))
+            (parity-list (cdr s)))))
+  (append (list first) (parity-list rest)))
+
+(define (square-list items)
+  (if (null? items)
+      '()
+      (cons (square (car items))  (square-list (cdr items)))))
+
+(define (square-list2 items)
+  (map square items))
+
+(define (square-list3 items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things) 
+              (cons (square (car things))
+                    answer))))
+  (iter items nil))
+
+(define (square-list4 items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons answer
+                    (square (car things))))))
+  (iter items nil))
+
+;;(define (for-each proc s)
