faa5d164e97c5ee08861aa4371197b7447856474
Glen Kaukola <gkaukola@cs.ucr.edu>
2013-08-05 22:44:22 -0700

    Sets

---
+++ section2.3/2_60.scm
@@ -0,0 +1,23 @@
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cons x set))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)        
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (union-set (adjoin-set (car set2) set1) (cdr set2)))))
+
+(define set1 (list 1 2 3 4 5))
+(define set2 (list 3 4 5 6 7 8 9))
+
