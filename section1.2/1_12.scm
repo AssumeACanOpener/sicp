29b8038adf14060ee9e71923faeeab68127bbe25
Glen Kaukola <gkaukola@cs.ucr.edu>
2019-07-27 20:05:07 -0700

    Man, I was overthinking 1.12 before.

--- section1.2/1_12.scm
+++ section1.2/1_12.scm
@@ -1,7 +1,5 @@
-(define (pascal row col)
-  (cond ((< row 1) 0)
-        ((= col 1) 1)
-        ((= col row) 1)
-        ((< col 1) 0)
-        ((> col row) 0)
-        (else (+ (pascal (- row 1) (- col 1)) (pascal (- row 1) col)))))
+(define (pascal-triangle row column)
+  (if (or (<= column 1) (>= column row))
+      1
+      (+ (pascal-triangle (- row 1) (- column 1))
+         (pascal-triangle (- row 1) column))))
