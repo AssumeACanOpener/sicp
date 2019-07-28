1444c9901cc42472554acfbe3bff7c75af3b3f08
Glen Kaukola <gkaukola@cs.ucr.edu>
2019-07-27 19:49:33 -0700

    Better names

--- section1.2/1_11.scm
+++ section1.2/1_11.scm
@@ -1,9 +1,9 @@
-(define (procedure-1-11 n)
+(define (recursive-1-11 n)
   (if (< n 3)
       n
-      (+ (procedure-1-11 (- n 1))
-         (* 2 (procedure-1-11 (- n 2)))
-         (* 3 (procedure-1-11 (- n 3))))))
+      (+ (recursive-1-11 (- n 1))
+         (* 2 (recursive-1-11 (- n 2)))
+         (* 3 (recursive-1-11 (- n 3))))))
 
 (define (iterative-1-11 n)
   (define (iter f-3 f-2 f-1 count)
