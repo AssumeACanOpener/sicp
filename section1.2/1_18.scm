fbfc0e91670671d3674d69cb33764e8e8cd30ab9
Glen Kaukola <gkaukola@cs.ucr.edu>
2019-12-22 14:55:46 -0800

    Better function name for 1.18

--- section1.2/1_18.scm
+++ section1.2/1_18.scm
@@ -1,8 +1,8 @@
-(define (mult a b)
-  (define (iter a b c)
-    (cond ((= b 0) c) 
-          ((even? b) (iter (double a) (halve b) c)) 
-          (else (iter a (- b 1) (+ c a)))))
+(define (iterative-fast-* a b)
+  (define (iter a b result)
+    (cond ((= b 0) result)
+          ((even? b) (iter (double a) (halve b) result))
+          (else (iter a (- b 1) (+ result a)))))
   (iter a b 0))
 
 (define (double n)
