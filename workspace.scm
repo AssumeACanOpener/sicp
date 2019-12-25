8da0ebf9a6b0b1836916ace4e703b16f2f12b36e
Glen Kaukola <gkaukola@cs.ucr.edu>
2019-12-24 17:38:58 -0800

    Up to 1.22

--- workspace.scm
+++ workspace.scm
@@ -200,3 +200,33 @@
                         p
                         q
                         (- count 1)))))
+
+;;;;;;;;;;;;;;;;;
+; exercise 1.22 ;
+;;;;;;;;;;;;;;;;;
+(define (prime? n)
+  (= n (smallest-divisor n)))
+(define (smallest-divisor n) (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (search-for-primes start finish)
+  (timed-prime-test start)
+  (if (>= start finish)
+      (begin (newline)
+             (display "Done"))
+      (search-for-primes (+ start 2) finish)))
