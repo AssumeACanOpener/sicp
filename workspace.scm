43690483c838fa765c12f1ec6f97288e8d128cab
Glen Kaukola <gkaukola@cs.ucr.edu>
2019-12-28 17:30:06 -0800

    Up to problem 1.24.

--- workspace.scm
+++ workspace.scm
@@ -219,22 +219,56 @@
 
   (= n (smallest-divisor n)))
 
-(define (timed-prime-test n)
-  (newline)
-  (display n)
-  (start-prime-test n (runtime)))
-
-(define (start-prime-test n start-time)
-  (if (prime? n)
-      (report-prime (- (runtime) start-time))))
-
-(define (report-prime elapsed-time)
-  (display " *** ")
-  (display elapsed-time))
-
 (define (search-for-primes start finish)
+
+  (define (report-prime elapsed-time)
+    (display " *** ")
+    (display elapsed-time))
+
+  (define (start-prime-test n start-time)
+    (if (prime? n) (report-prime (- (runtime) start-time))))
+
+  (define (timed-prime-test n)
+    (newline)
+    (display n)
+    (start-prime-test n (runtime)))
+
   (timed-prime-test start)
   (if (>= start finish)
       (begin (newline)
              (display "Done"))
       (search-for-primes (+ start 2) finish)))
+
+;;;;;;;;;;;;;;;;;
+; exercise 1.24 ;
+;;;;;;;;;;;;;;;;;
+(define (fast-prime? n times)
+
+  (define (square x) (* x x))
+
+  (define (expmod base exp m)
+    (cond ((= exp 0) 1)
+          ((even? exp) (remainder (square (expmod base (/ exp 2) m)) m))
+          (else (remainder (* base (expmod base (- exp 1) m)) m))))
+
+  (define (fermat-test n)
+    (define (try-it a)
+      (= (expmod a n n) a))
+    (try-it (+ 1 (random (- n 1)))))
+
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(define (timed-fast-prime n)
+
+  (define (report-prime elapsed-time)
+    (display " *** ")
+    (display elapsed-time))
+
+  (define (start-prime-test n start-time)
+    (if (fast-prime? n 100) (report-prime (- (runtime) start-time))))
+  
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
