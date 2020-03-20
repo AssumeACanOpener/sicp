4d737d5db86efe84928f76d79c36b9d8a471bd27
Glen Kaukola <gkaukola@cs.ucr.edu>
2020-03-19 22:52:46 -0700

    A more elegant recursive solution to 1.37 this time.  In my opinion anyhow.

--- section1.3/1_37.scm
+++ section1.3/1_37.scm
@@ -1,17 +1,19 @@
 #lang sicp
 
-(define (cont-frac n d k)
-  (define (iter i)
-    (if (= i k)
-        (/ (n i) (d i))
-        (/ (n i) (+ (d i) (iter (+ i 1))))))
-  (iter 1))
-
 (define (golden-ratio k)
   (/ 1
      (cont-frac (lambda (i) 1.0)
                 (lambda (i) 1.0)
                  k)))
+
+(define (cont-frac n d k)
+  (if (= k 1)
+      (/ (n 1) (d 1))
+      (/ (n 1)
+         (+ (d 1)
+            (cont-frac (lambda (x) (n (+ x 1)))
+                       (lambda (x) (d (+ x 1)))
+                       (- k 1))))))
 
 (define (golden-ratio-iter k)
   (/ 1
