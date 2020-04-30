f9d6fda466492c62aea885f1d33877dd8ed77384
Glen Kaukola <gkaukola@cs.ucr.edu>
2020-04-30 13:59:32 -0700

    More progress.

--- section1.3/1_39.scm
+++ section1.3/1_39.scm
@@ -1,16 +1,19 @@
 #lang sicp
 
-(define (cont-frac n d k)
-  (define (iter i)
-    (if (= i k)
-        (/ (n i) (d i))
-        (/ (n i) (+ (d i) (iter (+ i 1))))))
-  (iter 1))
+(define (tan-cf x k)
+  (let ((xsq (* x x)))
+    (cont-frac (lambda (i)
+                 (if (= i 1)
+                     x
+                     (- xsq)))
+               (lambda (i) (- (* i 2) 1))
+               k)))
 
-(define (tan x k)
-   (cont-frac (lambda (i)
-                (if (= 1 i)
-                x
-                (- (* x x))))
-              (lambda (i) (- (* i 2) 1))
-               k))
+(define (cont-frac n d k)
+  (if (= k 1)
+      (/ (n 1) (d 1))
+      (/ (n 1)
+         (+ (d 1)
+            (cont-frac (lambda (x) (n (+ x 1)))
+                       (lambda (x) (d (+ x 1)))
+                       (- k 1))))))
