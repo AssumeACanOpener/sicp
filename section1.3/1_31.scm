ef2a48ddcf4eac3a5b73ce3d7da03784fa60a3c8
Glen Kaukola <gkaukola@cs.ucr.edu>
2020-11-14 13:10:51 -0800

    Experimenting with an iterative vs. recursive process.

--- section1.3/1_31.scm
+++ section1.3/1_31.scm
@@ -7,22 +7,24 @@
       (* (term a)
          (product term (next a) next b))))
 
-(define (compute-pi num-terms)
-  (define (pi-term n)
-    (if (even? n)
-        (/ n (+ n 1))
-        (/ (+ n 1) n)))
-  (* 4.0
-     (product pi-term 2 inc num-terms)))
-
-(define pi (compute-pi 10000))
-
-(define (factorial n)
-  (product identity 1 inc n))
-
 (define (iter-product term a next b)
   (define (iter current result)
     (if (> current b)
         result
         (iter (next current) (* (term current) result))))
   (iter a 1))
+
+(define (compute-pi num-terms)
+  (define (pi-term n)
+    (if (even? n)
+        (/ n (+ n 1))
+        (/ (+ n 1) n)))
+  (* 4.0
+     (iter-product pi-term 2 inc num-terms)))
+
+(define pi (compute-pi 10000))
+
+(define (factorial n)
+  (product identity 1 inc n))
+
+
