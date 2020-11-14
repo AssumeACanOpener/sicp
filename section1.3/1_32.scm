0d511a316794c421b8b901047e3c6de114b30f7a
Glen Kaukola <gkaukola@cs.ucr.edu>
2020-11-14 13:16:26 -0800

    Simplified the iterative accumulate

--- section1.3/1_32.scm
+++ section1.3/1_32.scm
@@ -8,17 +8,17 @@
                 (accumulate combiner null-value term (next a) next b))))
 
 (define (accumulate-iterative combiner null-value term a next b)
-  (define (iter combiner term next a result)
-    (if (> a b)
+  (define (iter current result)
+    (if (> current b)
         result
-        (iter combiner term next (next a) (combiner (term a) result))))
-  (iter combiner term next a null-value))
+        (iter (next current) (combiner (term current) result))))
+  (iter a null-value))
 
 (define (product term a next b)
   (accumulate-iterative * 1 term a next b))
 
 (define (sum term a next b)
-  (accumulate + 0 term a next b))
+  (accumulate-iterative + 0 term a next b))
 
 (define (factorial n)
   (product identity 1 inc n))
