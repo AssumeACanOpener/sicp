78180533f9fca22c0aa4265ac6bc0f8a61d83532
Glen Kaukola <gkaukola@cs.ucr.edu>
2020-11-20 15:10:57 -0800

    Broke 1.38 out in to helper functions for the numerator and denominator.

--- section1.3/1_38.scm
+++ section1.3/1_38.scm
@@ -1,13 +1,12 @@
 #lang sicp
 
-(define (aproximate-e k)
-  (+ 2
-     (cont-frac (lambda (x) 1.0)
-                (lambda (x)
-                  (if (= (modulo (+ x 1) 3) 0)
-                      (* (/ (+ x 1) 3) 2)
-                      1.0))
-                k)))
+(define (aprox-e num-terms)
+  (define (numer x) 1.0)
+  (define (denom x)
+    (if (= (modulo (+ x 1) 3) 0)
+        (* (/ (+ x 1) 3.0) 2.0)
+        1.0))
+  (+ 2 (cont-frac numer denom num-terms)))
 
 (define (cont-frac n d k)
   (if (= k 1)
