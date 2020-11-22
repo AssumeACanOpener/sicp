d507ff48303e7bdb70aa220f85b68eee21688318
Glen Kaukola <gkaukola@cs.ucr.edu>
2020-11-22 13:52:35 -0800

    Fixed 2.27 bug.

--- section2.2/2_27.scm
+++ section2.2/2_27.scm
@@ -1,13 +1,12 @@
 #lang sicp
 
 (define (deep-reverse s)
-  (cond ((null? (cdr s))
-          s)
-        ((list? (car s))
-          (append (deep-reverse (cdr s))
-                  (list (deep-reverse (car s)))))
+  (cond ((null? s) s)
+        ((pair? (car s))
+         (append (deep-reverse (cdr s))
+                 (list (deep-reverse (car s)))))
         (else
-          (append (deep-reverse (cdr s))
-                  (list (car s))))))
+         (append (deep-reverse (cdr s))
+                 (list (car s))))))
 
 (define a (list (list 1 2) (list 3 4)))
