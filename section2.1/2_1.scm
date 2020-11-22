8e7c40b4fdfda063e3bf69b5693ec5e356003a5c
Glen Kaukola <gkaukola@cs.ucr.edu>
2020-11-22 09:57:09 -0800

    gcd is a primitive function.

--- section2.1/2_1.scm
+++ section2.1/2_1.scm
@@ -9,11 +9,6 @@
   (display "/")
   (display (denom x)))
 
-(define (gcd a b)
-  (if (= b 0)
-      a
-      (gcd b (remainder a b))))
-
 (define (make-rat n d) 
   (let ((g ((if (< d 0) - +) (gcd n d)))) 
     (cons (/ n g) (/ d g))))
