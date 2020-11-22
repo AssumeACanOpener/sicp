60359650711eb111fa67a524308144a521eb38c8
Glen Kaukola <gkaukola@cs.ucr.edu>
2020-11-22 13:57:16 -0800

    2.28, cond is cleaner.

--- section2.2/2_28.scm
+++ section2.2/2_28.scm
@@ -2,9 +2,9 @@
 
 (define x (list (list 1 2) (list 3 4)))
 
-(define (fringe input)
-  (if (null? input)
-      input
-      (if (pair? (car input))
-        (append (fringe (car input)) (fringe (cdr input)))
-        (append (list (car input)) (fringe (cdr input))))))
+(define (fringe s)
+  (cond ((null? s) s)
+        ((pair? (car s))
+         (append (fringe (car s)) (fringe (cdr s))))
+        (else
+         (append (list (car s)) (fringe (cdr s))))))
